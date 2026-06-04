5bc728efb8d7e5291828486d5dc2ac029a1ce479
mysakashd-png <mys.akash.d@gmail.com>
2026-06-05 01:29:13 +0530

    Update 3_Species_Analysis.py

--- pages/3_Species_Analysis.py
+++ pages/3_Species_Analysis.py
@@ -1,46 +1,70 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from utils.mappings import load_species_mapping
 
-species_map = load_species_mapping()
+from utils.mappings import (
+    load_species_mapping,
+    load_country_mapping
+)
 
-st.title("🐟 Species Analysis")
-
+# -----------------------------
+# Load Data
+# -----------------------------
 df = pd.read_csv(
     "data/raw/Aquaculture_Quantity.csv"
 )
 
+species_map = load_species_mapping()
+country_map = load_country_mapping()
 
+st.title("🐟 Species Analysis")
 
+# -----------------------------
+# Species Dropdown
+# -----------------------------
 species_list = sorted(
-    df["SPECIES.ALPHA_3_CODE"].unique()
+    df["SPECIES.ALPHA_3_CODE"]
+    .dropna()
+    .astype(str)
+    .unique()
 )
 
 selected_species = st.selectbox(
     "Select Species",
     species_list,
-    format_func=lambda x:
+    format_func=lambda x: str(
         species_map.get(x, x)
+    )
 )
 
-
+# -----------------------------
+# Filter Data
+# -----------------------------
 species_df = df[
     df["SPECIES.ALPHA_3_CODE"]
+    .astype(str)
     == selected_species
 ]
 
+# -----------------------------
+# Production Trend
+# -----------------------------
 trend = (
     species_df.groupby("PERIOD")["VALUE"]
     .sum()
     .reset_index()
 )
 
+species_name = species_map.get(
+    selected_species,
+    selected_species
+)
+
 fig = px.line(
     trend,
     x="PERIOD",
     y="VALUE",
-    title=f"{selected_species} Production Trend"
+    title=f"{species_name} Production Trend"
 )
 
 st.plotly_chart(
@@ -48,6 +72,9 @@ st.plotly_chart(
     use_container_width=True
 )
 
+# -----------------------------
+# Top Producing Countries
+# -----------------------------
 country_data = (
     species_df.groupby(
         "COUNTRY.UN_CODE"
@@ -61,11 +88,16 @@ country_data = (
     .head(10)
 )
 
+country_data["Country"] = (
+    country_data["COUNTRY.UN_CODE"]
+    .map(country_map)
+)
+
 fig2 = px.bar(
     country_data,
-    x="COUNTRY.UN_CODE",
+    x="Country",
     y="VALUE",
-    title="Top Producing Countries"
+    title=f"Top Countries Producing {species_name}"
 )
 
 st.plotly_chart(
